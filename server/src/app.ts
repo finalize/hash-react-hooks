e6b8a8f984406b479b5163b48deff729d6391082
shgysd <shgysd@gmail.com>
2019-05-22 01:47:14 +0900

    Setup server

---
+++ server/src/app.ts
@@ -0,0 +1,21 @@
+import * as express from "express";
+import * as path from "path";
+import * as cookieParser from "cookie-parser";
+import * as logger from "morgan";
+
+var indexRouter = require("../routes/index");
+var usersRouter = require("../routes/users");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("*", (req: any, res: any) => {
+  res.sendFile(path.join(__dirname + "/../public/index.html"));
+});
+
+export = app;
